72c46696b47185862a07a8aad3fb672f3c114a21
Vit Benton <vitbenton@Macintosh.local>
2017-06-19 22:58:48 -0400

    v2.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,44 @@
-//if (true){
-//document.getElementById("wins").innerHTML = "new HTML";
-//}
-//use to edit existing html in ID ^
+//object begins
+var game = {
+	wins: 0,
+	guesses: 6,
+	lettersGuessed: [],
+	texas: ["_","_","_","_","_"],
+	florida: ["_","_","_","_","_","_","_"],
+	maine: ["_","_","_","_","_"],
+
+	//end of variables
+
+	startUp: window.onload = function(pageLoad) {
+		//loading initial values
+    document.getElementById("winsValue").innerHTML = game.wins;
+    document.getElementById("guessesValue").innerHTML = game.guesses;
+    document.getElementById("lettersGuessedList").innerHTML = game.lettersGuessed;
+    var tex = game.texas[0] + game.texas[1] + game.texas[2] + game.texas[3] + game.texas[4];
+    document.getElementById("password").innerHTML = tex;
+
+	},
+
+
+	startUp: document.onkeyup = function(guessList) {
+		//unlock t letter
+		var keyChoice = event.key;
+
+        if (keyChoice === "t"){
+          game.lettersGuessed.push("T");
+        }
+
+    },
+
+
+// end of object
+};
+
+
+
+
+
+
+
+
+
