8319bd68d94c2186f4050f6370e4085c44ae9c75
Vit Benton <vitbenton@Macintosh.local>
2017-06-21 20:58:54 -0400

    V3.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,115 @@
 //object begins
-var game = {
+var hangman = {
+
+	//PROPERTIES:
 	wins: 0,
-	guesses: 6,
-	lettersGuessed: [],
-	texas: ["_","_","_","_","_"],
-	florida: ["_","_","_","_","_","_","_"],
-	maine: ["_","_","_","_","_"],
+	guessesAllow: 10,
+	passwords: ["texas","florida","washington","georgia","alabama","alaska","idaho","virginia","michigan","california","wisconsin","oregon","nevada","oklahoma","hawaii","minnesota","kentucky","pennsylvania","massachusetts","illinois","colorado","tennessee"],
+    selectedPassword:"",
+    selectedPasswordArray: [],
+	guesses: [],
+	hiddenPassword: [],
 
-	//end of variables
+	//----------------END OF PROPERTIES
 
-	startUp: window.onload = function(pageLoad) {
-		//loading initial values
-    document.getElementById("winsValue").innerHTML = game.wins;
-    document.getElementById("guessesValue").innerHTML = game.guesses;
-    document.getElementById("lettersGuessedList").innerHTML = game.lettersGuessed;
-    var tex = game.texas[0] + game.texas[1] + game.texas[2] + game.texas[3] + game.texas[4];
-    document.getElementById("password").innerHTML = tex;
+	//METHODS:
 
+	passGen: function(mkPass){
+
+	    var randNum = Math.floor(Math.random() * hangman.passwords.length);
+	    hangman.selectedPassword = hangman.passwords[randNum];
+	    hangman.selectedPasswordArray = hangman.selectedPassword.split(""); 
+	    console.log("current password is: " + hangman.selectedPassword);
 	},
 
 
-	startUp: document.onkeyup = function(guessList) {
-		//unlock t letter
-		var keyChoice = event.key;
+	HiddenPasswordGen: function(mkHidPass){
+    	for (var i = 0; i < hangman.selectedPassword.length; i++) {
 
-        if (keyChoice === "t"){
-          game.lettersGuessed.push("T");
-        }
+    		hangman.hiddenPassword.push("_");
+    	};
 
-    },
+        console.log(hangman.hiddenPassword);
+	},
 
+	subGuesses: function(subtractGuesses){
+		hangman.guessesAllow--;
+	},
 
-// end of object
+	addWins: function(addToWins){
+		hangman.wins++
+	},
+
+	scoreCheck: function(checkForWin){
+    	var joinedPass = hangman.selectedPasswordArray.join("");
+    	var joinedHiddenPass = hangman.hiddenPassword.join("");
+
+    	if (joinedPass == joinedHiddenPass) {
+	        confirm("YOU GOT IT!");
+	        hangman.wins++;
+	        console.log("Your new score is: " + hangman.wins);
+	        hangman.guesses = [];
+	        hangman.hiddenPassword = [];
+	        hangman.selectedPassword="";
+	        hangman.selectedPasswordArray = [];
+	        hangman.passGen();
+	        hangman.HiddenPasswordGen();
+    	};
+	},
+
+    resetGame: function(reset){
+        hangman.guesses = [];
+        hangman.hiddenPassword = [];
+        hangman.guessesAllow = 10;
+        hangman.passGen();
+        hangman.HiddenPasswordGen();
+	},
+	//----------------END OF METHODS
+
 };
+// end of object
 
+window.onload = function(load){
 
+    hangman.passGen();
 
+    hangman.HiddenPasswordGen();
 
+    console.log("Your wins: " + hangman.wins);
 
+    console.log("Incorrect guesses left: " + hangman.guessesAllow);
+    };
 
+document.onkeypress = function(startGame){
+	var keyChoice = event.key;
+	var missed = true;
+	hangman.guesses.push(keyChoice);
 
+	for (var i = 0; i < hangman.selectedPassword.length; i++) {
 
+    	if (keyChoice == hangman.selectedPassword.charAt(i)){
+        	hangman.hiddenPassword[i] = hangman.selectedPassword[i];
+            missed = false;
+        };
+    };
 
+    if (missed){
+    	hangman.subGuesses();
+    };
+
+    if (hangman.guessesAllow === 0){
+    	var confLoss = confirm("GAME OVER: You ran out of guesses!");
+     };
+
+    if (confLoss == true){
+        hangman.resetGame();
+    };
+
+    console.log("Your guesses: " + hangman.guesses);
+    console.log(hangman.hiddenPassword);
+    console.log("Your wins: " + hangman.wins);
+    console.log("You now have " + hangman.guessesAllow + " wrong guesses left");
+
+    hangman.scoreCheck();
+
+};
